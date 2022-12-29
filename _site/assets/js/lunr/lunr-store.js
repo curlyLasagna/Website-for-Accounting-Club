469651e1eb6245e2d55c0f1508c3aa13a20cd8e5
John Doe <johndoe@Renzos-MBP.fios-router.home>
2022-12-28 23:20:56 -0500

    deleted files from old main

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "First post of the club!",
-        "excerpt":"In this post, we will talk about the first post I’ve ever posted and the very post I’ve posted that is the firstest of the first  ","categories": [],
-        "tags": [],
-        "url": "/2022/12/17/welcome-to-jekyll.html",
-        "teaser": null
-      }]
